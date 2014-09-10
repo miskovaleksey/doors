e9211ae93247ed0a6c229021fb1a6e1141d48d6d
Aleskey Miskov <miskovaleksey@gmail.com>
2014-09-10 19:34:08 +0600

    added hero block

--- javascript/doors.js
+++ javascript/doors.js
@@ -15,24 +15,6 @@ jQuery(function(){
     $('#popup').modal('show');
   })
 
-  // var popup = new PopupSimple('#form-popup');
-
-  // $('.types a').click(function(){
-  //   popup.show();
-  //   var parentElement = $(this).parents('.wrap-item');
-  //   var htmlForPopup  = $(parentElement).find('.block-for-popup').html();
-
-  //   $('#popup-simple-content').html(htmlForPopup);
-  //   new Carousel('#popup-simple-wraper .wrapper-images');
-  //   popup.center();
-
-  //   $('#popup-simple-wraper .item').each(function(index, element){
-  //     console.log(element);
-  //     if (index == 0) {
-  //       $(element).addClass('active');
-  //     };
-  //   });
-
   //   $('#popup-simple-wraper .item').click(function () {
   //     $('#popup-simple-wraper .item').removeClass('active');
   //     setActiveImage($(this));
