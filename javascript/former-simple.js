f074e7069ed5dd313dafc1650664258a69c19e6c
Aleskey Miskov <miskovaleksey@gmail.com>
2014-09-14 17:33:17 +0600

    added mailer.php
    fixed style send form

---
+++ javascript/former-simple.js
@@ -0,0 +1,66 @@
+jQuery(function($){
+$('body').on('submit','form',function(e){
+  e.preventDefault();
+
+  var url = $(this).attr('action');
+  var data = [];
+  var raw = $(this).serializeArray();
+  var form = $(this);
+  var has_errors = false;
+
+  if(form.hasClass('loading')){
+    return false;
+  }
+
+  form.find('.error').html("");
+
+  for(i in raw){
+    var name = raw[i].name;
+    var field = $(this).find('[name="'+name+'"]');
+    var value = field.val();
+    var required = field.data('required');
+
+    if(required && (!value || value.length==0)){
+      if(form.find('.error').length == 0){//legacy
+        alert('Не заполнено поле "'+field.data('name')+'"');
+      }else{
+        field.parents('.form-group').find('.error').html("Поле должно быть заполнено");
+      }
+      field.focus();
+      has_errors = true;
+    }
+  }
+
+  if(!has_errors){
+    form.addClass('loading');
+    $('form').parents('.modal-content').find('.modal-title').css('display', 'none')
+    $.ajax(url,{
+      url: url,
+      type: "POST",
+      data: form.serialize(),
+      success: function(){
+        try{
+          yaCounter.reachGoal(form.attr('data-metrika-goal'));
+        }catch(e){}
+
+        form.addClass('success');
+
+        if(form.find('.loader').length == 0){//legacy
+          alert('Спасибо за заявку!')
+        }
+      },
+
+      error: function(){
+        form.addClass('error');
+        alert('Произошла ошибка, повторите попытку позже');
+      },
+
+      complete: function(){
+        form.removeClass('loading');
+        form.find('input[type=\'text\']').val('');
+      }
+    });
+  }
+  return false;
+});
+});
---
+++ mailer.php
@@ -0,0 +1,49 @@
+<?php
+date_default_timezone_set('UTC');
+
+$order = $_POST['order'];
+
+$body='';
+foreach($order as $key => $value){
+  $body .=
+    '<tr>'
+      .'<td>'.$key.'</td>'
+      .'<td>'.$value.'</td>'
+   .'</tr>';
+}
+$body ='<table>'.$body.'</table>';
+
+require_once "PHPMailer/PHPMailerAutoload.php";
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com;';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'miskovaleksey@gmail.com';                 // SMTP username
+$mail->Password = 'Miskov_83';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;
+$mail->CharSet = 'UTF-8';
+
+$mail->From = 'from@example.com';
+$mail->FromName = 'Mailer';
+$mail->addAddress('alien2005@e1.ru');     // Add a recipient
+// $mail->addAddress('maksim@context-prosto.ru');     // Add a recipient
+$mail->addReplyTo('info@example.com', 'Information');
+
+$mail->WordWrap = 50;                                 // Set word wrap to 50
+$mail->isHTML(true);                                  // Set email format to
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = $body;
+
+if(!$mail->send()) {
+    header('Content-Type: text/json', true, 500);
+    echo json_encode(Array('error' => $mail->ErrorInfo));
+} else {
+    header('Content-Type: text/json');
+    echo json_encode(Array('error' => false));
+}
